4fabb6d20848eda8958d4a074c4096b079dcad20
olina411 <olina.liang@prismsus.org>
2017-11-16 18:45:52 -0500

    Nothing changed

--- Re_Energy_Shiny/app.R
+++ Re_Energy_Shiny/app.R
@@ -1,6 +1,9 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(ggmap)
+library(maps)
+library(mapdata)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -35,6 +38,11 @@ server <- function(input, output) {
       
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+   output$Map <- renderPlot({
+     usa <- map_data("usa") # we already did this, but we can do it again
+     ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
+       coord_fixed(1.3)
    })
 }
 
