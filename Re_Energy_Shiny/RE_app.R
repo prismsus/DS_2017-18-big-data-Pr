76eb34a69996e65df68d97bc293a905eb6c365af
Gary <32275864+chenggaryguo1206@users.noreply.github.com>
2017-11-16 18:40:23 -0500

    Delete Useless Comments

--- Re_Energy_Shiny/RE_app.R
+++ Re_Energy_Shiny/RE_app.R
@@ -2,13 +2,15 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-# Define UI for application that draws a histogram
+
+
+# UI
 RE_ui <- fluidPage(
    
    # Application title
    titlePanel("Renewable Energy Electricity Generation in Each Year"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar
    sidebarLayout(
       sidebarPanel(
          sliderInput("Years",
@@ -16,6 +18,7 @@ RE_ui <- fluidPage(
                      min = 2005,
                      max = 2015,
                      value = 2010)
+         selectInput
       ),
       
       # Show a plot of the generated distribution
@@ -25,15 +28,12 @@ RE_ui <- fluidPage(
    )
 )
 
-# Define server logic required to draw a histogram
+# Server
 RE_server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
       Years <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 }
